0dc818279d06924a03e2c6bb264d49115424b80d
Ashutosh Dash <dasashu2004@gmail.com>
2026-06-09 15:48:17 +0530

    Render deployment configuration

--- electron/main.cjs
+++ electron/main.cjs
@@ -5,6 +5,8 @@ function createWindow() {
   const win = new BrowserWindow({
     width: 1400,
     height: 900,
+    title: "SEWAC Admin Dashboard",
+    icon: path.join(__dirname, "../build/sewac-helper.ico"),
     autoHideMenuBar: true,
 
     webPreferences: {
--- src/services/api.js
+++ src/services/api.js
@@ -1,4 +1,4 @@
 const API_BASE_URL =
-  import.meta.env.VITE_API_URL;
+  import.meta.env.VITE_API_URL || "http://18.60.41.32:5000";
 
 export default API_BASE_URL;
